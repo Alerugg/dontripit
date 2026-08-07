00c542de32bdb126947e69b2ba10e658b6665669
Alejandro Ruggeri <134484194+Alerugg@users.noreply.github.com>
2026-08-07 21:23:00 +0200

    Route One Piece to dedicated Search V2 explorer

--- frontend/app/games/[slug]/page.js
+++ frontend/app/games/[slug]/page.js
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation'
 import TopNav from '../../../components/layout/TopNav'
 import GameExplorerPage from '../../../components/games/GameExplorerPage'
+import OnePieceExplorerV2Page from '../../../components/games/OnePieceExplorerV2Page'
 import { getGameConfig } from '../../../lib/catalog/games'
 
 export default function GamePage({ params }) {
@@ -11,7 +12,11 @@ export default function GamePage({ params }) {
   return (
     <main>
       <TopNav />
-      <GameExplorerPage game={game} />
+      {game.slug === 'onepiece' ? (
+        <OnePieceExplorerV2Page game={game} />
+      ) : (
+        <GameExplorerPage game={game} />
+      )}
     </main>
   )
 }
