7bc7ef4d7664bb925e7c4503a4e35a6228e0e565
Alejandro Ruggeri <134484194+Alerugg@users.noreply.github.com>
2026-08-08 18:53:06 +0200

    Route Yu-Gi-Oh hub through Search V2

--- frontend/app/games/[slug]/page.js
+++ frontend/app/games/[slug]/page.js
@@ -3,6 +3,7 @@ import TopNav from '../../../components/layout/TopNav'
 import GameExplorerPage from '../../../components/games/GameExplorerPage'
 import OnePieceExplorerV2Page from '../../../components/games/OnePieceExplorerV2Page'
 import PokemonExplorerV2Page from '../../../components/games/PokemonExplorerV2Page'
+import YugiohExplorerV2Page from '../../../components/games/YugiohExplorerV2Page'
 import { getGameConfig } from '../../../lib/catalog/games'
 
 export default async function GamePage({ params }) {
@@ -14,6 +15,7 @@ export default async function GamePage({ params }) {
   let explorer = <GameExplorerPage game={game} />
   if (game.slug === 'onepiece') explorer = <OnePieceExplorerV2Page game={game} />
   if (game.slug === 'pokemon') explorer = <PokemonExplorerV2Page game={game} />
+  if (game.slug === 'yugioh') explorer = <YugiohExplorerV2Page game={game} />
 
   return (
     <main>
